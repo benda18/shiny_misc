c0d8b34964da13d5f672af2d459f7fd4cf013536
Tim Bender <bendertj@gmail.com>
2023-10-03 12:50:11 -0400

    Add files via upload

--- shiny_nc.coastal.hazards/R/coastal_hazard_data_explore.R
+++ shiny_nc.coastal.hazards/R/coastal_hazard_data_explore.R
@@ -8,8 +8,37 @@ setwd("~/R/play/shiny_misc/shiny_nc.coastal.hazards")
 
 # wd management----
 
-wd.list <- list(data = "C:/Users/bende/Documents/R/play/shiny_misc/shiny_nc.coastal.hazards/data", 
-                metadata = "C:/Users/bende/Documents/R/play/shiny_misc/shiny_nc.coastal.hazards/metadata", 
-                R = "C:/Users/bende/Documents/R/play/shiny_misc/shiny_nc.coastal.hazards/R", 
-                output = "C:/Users/bende/Documents/R/play/shiny_misc/shiny_nc.coastal.hazards/output", 
-                shiny = NA)
+wd <- list(data = "C:/Users/bende/Documents/R/play/shiny_misc/shiny_nc.coastal.hazards/data", 
+           data.flood = "C:/Users/bende/Documents/R/play/shiny_misc/shiny_nc.coastal.hazards/data/nc_flood_2023_csvtables",
+           data.groundwater = "C:/Users/bende/Documents/R/play/shiny_misc/shiny_nc.coastal.hazards/data/nc_groundwater_2023_csvtables",
+           data.shoreline = "C:/Users/bende/Documents/R/play/shiny_misc/shiny_nc.coastal.hazards/data/nc_shorelin_change_2023_csvtables",
+           metadata = "C:/Users/bende/Documents/R/play/shiny_misc/shiny_nc.coastal.hazards/metadata", 
+           R = "C:/Users/bende/Documents/R/play/shiny_misc/shiny_nc.coastal.hazards/R", 
+           output = "C:/Users/bende/Documents/R/play/shiny_misc/shiny_nc.coastal.hazards/output", 
+           shiny = NA, 
+           home = "C:/Users/bende/Documents/R/play/shiny_misc/shiny_nc.coastal.hazards")
+
+
+# load data----
+
+setwd(wd$data.flood)
+flood_df.list <- list()
+  for(i in list.files()){
+    flood_df.list[[i]] <- read_csv(i)
+  }
+
+
+setwd(wd$data.shoreline)
+shoreline_df.list <- list()
+for(i in list.files()){
+  shoreline_df.list[[i]] <- read_csv(i)
+}
+rm(i)
+
+setwd(wd$metadata)
+assets_codes <- read_csv("assets_codes.csv")
+
+setwd(wd$home)
+
+
+# explore data----
