5ac3c1b36d3e4f39cafdee589c72aff6876e6b9c
Tim Bender <bendertj@gmail.com>
2023-10-06 15:04:22 -0400

    Add files via upload

--- shiny_tornados/shiny/shiny_tornadoes/app.R
+++ shiny_tornados/shiny/shiny_tornadoes/app.R
@@ -13,7 +13,10 @@ load("tornado.RData")
 #   mutate(., 
 #          COUNTYFP = as.numeric(COUNTYFP))
 
-actualT <- left_join(actualT, cw_magnitude) %>% as_tibble()
+actualT <- left_join(actualT, cw_magnitude) %>% 
+  as_tibble() %>%
+  mutate(., 
+         aes_color = loss)
 rm(cw_magnitude)
 
 # TODO - need to somehow join FIPS code for county to actualT and shapefile so
@@ -31,6 +34,10 @@ actualT_pts <- actualT %>%
 var.pointsize <- 0.1
 var.pointcolor <- "black"
 
+# temp-testing
+# input <- list(`sel_states` = c("OH"), `sel_counties` = c("Warren", "Butler", "Hamilton"), `sel_year` = c(1990,2020))
+#/temp-testing
+
 
 # UI----
 ui <- fluidPage(
@@ -42,21 +49,22 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       # FILTERS----
-      shiny::headerPanel("User Selections:"),
+      #shiny::headerPanel("User Selections:"),
+      h3(HTML(r'(<b>USER SELECTIONS:</b>)')),
       # Select State(s)
-      fluidRow(column(12),
+      fluidRow(column(12,
                shiny::selectInput(inputId = "sel_states", 
                                   label = "Select State(s)", 
                                   choices = reg_st_list,
                                   selected = c("OH", "IN", "KY"),
-                                  multiple = T)),
+                                  multiple = T))),
       # Select Counties
-      fluidRow(column(12),
+      fluidRow(column(12,
                shiny::selectInput(inputId = "sel_counties", 
                                   label = "Select Count(y/ies)", 
                                   choices = 'N/A',
                                   #selected = c("OH", "IN", "KY"),
-                                  multiple = T)),
+                                  multiple = T))),
       # Filter years
       fluidRow(column(12,
                       shiny::sliderInput(inputId = "sel_year", 
@@ -67,14 +75,24 @@ ui <- fluidPage(
                                             sep = "")
                       )),
       # Filter Magnitude
-      fluidRow(column(12), 
+      fluidRow(column(12, 
                shiny::sliderInput(inputId = "sel_magnitude", 
                                   label   = "Select Magnitude Range", 
                                   value   = c(min(actualT$def),max(actualT$def)), 
                                   min     = min(actualT$def), 
                                   max     = max(actualT$def), 
                                   sep = "", 
-                                  ticks = T))
+                                  ticks = T))), 
+      h3(HTML(r'(<b>AESTHETICS:</b>)')),
+      fluidRow(column(12, 
+                      shiny::radioButtons(inputId = "radio_color", 
+                                          label = "Color Tornado Paths By:", 
+                                          choices = list("Economic Loss" = "loss",
+                                                         "Crop Loss" = "closs",
+                                                         "# of Injuries" = "inj", 
+                                                         "# of Deaths" = "fat",
+                                                         "Magnitude" = "def"))
+                      ))
     ),
     mainPanel(
       plotOutput("plot01")
@@ -84,6 +102,27 @@ ui <- fluidPage(
 
 # SERVER----
 server <- function(input, output, session) {
+  
+  # observe({
+  #   # create an observe session where you create and update the
+  #   # actualT$aes_color var to match whatever the radioButton value is
+  #   
+  #   # # observe radioButton "radio_color" states
+  #   # obs_radio.color_states <- input$radio_color
+  #   
+  #   # # for SEGMENTS
+  #   # actualT_segs$aes_color <- select(actualT_segs, 
+  #   #                                  input$radio_color) %>% 
+  #   #   unlist() %>% 
+  #   #   unname()
+  #   # 
+  #   # # for POINTS
+  #   # actualT_pts$aes_color <- select(actualT_pts, 
+  #   #                                 input$radio_color) %>% 
+  #   #   unlist() %>% 
+  #   #   unname()
+  # })
+  
   
   observe({
     
@@ -114,6 +153,17 @@ server <- function(input, output, session) {
                                      tor.st_co_yr$COUNTY %in% input$sel_counties,]$uid_om.yr.st %>%
       unique()
     
+    # for SEGMENTS
+    actualT_segs$aes_color <- select(actualT_segs, 
+                                     input$radio_color) %>% 
+      unlist() %>% 
+      unname()
+    
+    # for POINTS
+    actualT_pts$aes_color <- select(actualT_pts, 
+                                    input$radio_color) %>% 
+      unlist() %>% 
+      unname()
     
     a.plot <- ggplot() + 
       theme(axis.ticks = element_blank(), 
@@ -138,7 +188,8 @@ server <- function(input, output, session) {
                                            between(x = actualT_segs$yr, 
                                                    min(input$sel_year), 
                                                    max(input$sel_year)),],
-                     aes(x = slon, xend = elon, y = slat,  yend = elat)) +
+                     aes(x = slon, xend = elon, y = slat,  yend = elat, 
+                         color = aes_color)) +
         geom_point(data = actualT_pts[actualT_pts$uid_om.yr.st %in% var_sel.uids & 
                                         between(x = actualT_pts$yr,
                                                 min(input$sel_year),
@@ -146,9 +197,9 @@ server <- function(input, output, session) {
                                         actualT_pts$slon != 0 & 
                                         actualT_pts$slat != 0,],
                    aes(x = slon, 
-                       y = slat), 
-                   size = var.pointsize, 
-                   color = var.pointcolor)+
+                       y = slat, 
+                       color = aes_color), 
+                   size = var.pointsize)+
         geom_point(data = actualT_pts[actualT_pts$uid_om.yr.st %in% var_sel.uids & 
                                         between(x = actualT_pts$yr, 
                                                 min(input$sel_year), 
@@ -156,9 +207,9 @@ server <- function(input, output, session) {
                                         actualT_pts$elon != 0 & 
                                         actualT_pts$elat != 0,],
                    aes(x = elon, 
-                       y = elat), 
-                   size = var.pointsize, 
-                   color = var.pointcolor)
+                       y = elat, 
+                       color = aes_color), 
+                   size = var.pointsize)
     }else{
       a.plot <- a.plot + 
         geom_sf(data = tigris_st_geo[tigris_st_geo$STUSPS %in% input$sel_states,], 
@@ -169,7 +220,8 @@ server <- function(input, output, session) {
                                            between(x = actualT_segs$yr, 
                                                    min(input$sel_year), 
                                                    max(input$sel_year)),], 
-                     aes(x = slon, xend = elon, y = slat,  yend = elat)) +
+                     aes(x = slon, xend = elon, y = slat,  yend = elat, 
+                         color = aes_color)) +
         geom_point(data = actualT_pts[actualT_pts$st %in% input$sel_states & 
                                         between(x = actualT_pts$yr, 
                                                 min(input$sel_year), 
@@ -177,9 +229,9 @@ server <- function(input, output, session) {
                                         actualT_pts$slon != 0 & 
                                         actualT_pts$slat != 0,],
                    aes(x = slon, 
-                       y = slat),
-                   size = var.pointsize, 
-                   color = var.pointcolor)+
+                       y = slat, 
+                       color = aes_color),
+                   size = var.pointsize)+
         geom_point(data = actualT_pts[actualT_pts$st %in% input$sel_states & 
                                         between(x = actualT_pts$yr, 
                                                 min(input$sel_year), 
@@ -187,18 +239,18 @@ server <- function(input, output, session) {
                                         actualT_pts$elon != 0 & 
                                         actualT_pts$elat != 0,],
                    aes(x = elon, 
-                       y = elat), 
-                   size = var.pointsize, 
-                   color = var.pointcolor)
+                       y = elat, 
+                       color = aes_color), 
+                   size = var.pointsize)
         
     }
     
-    a.plot
+    a.plot +
+      scale_color_viridis_c(name = input$radio_color,
+                            option = "C", 
+                            labels = scales::comma)
     
   })
-  
-  
-    
 }
 
 # Run the application 
