6b69fa817292a38574b5bfffe155368e2e583920
Tim Bender <bendertj@gmail.com>
2023-10-16 11:32:07 -0400

    Add files via upload

--- shiny_hownot2/R/img_2_tbl.R
+++ shiny_hownot2/R/img_2_tbl.R
@@ -34,34 +34,48 @@ img.url   <- "https://static.wixstatic.com/media/c990e4_1f596631067f4cf38c9e79a6
 # file.copy(from = paste0(img.title,".jpg"), to   = "table.jpg", 
 #           overwrite = ) # for processing
 
-file.copy("cross_loading_carabiners.png", "table.png")
+
 
 # convert to PDF
+file.copy("cross_loading_carabiners.jpg", "table.jpg", overwrite = T)
 
+library(jpeg)
+library(grid)
+image <- readJPEG("table.jpg")
+pdf("table.pdf", width = 8, height = 10)
+grid.raster(image)
+dev.off()
 
-# preprocessing
-img <- image_read("table.png") %>% 
-  image_crop(geometry_area(y = 35))  %>% 
-  image_transparent("white", fuzz=50) %>% 
-  image_background("white") %>%
-  image_negate() %>%
-  image_morphology(method = "Thinning", kernel = "Rectangle:20x1+0+0^<") %>%
-  image_negate()
+# read pdf
+#pdftools::pdf_ocr_data("table.pdf")
+x <- pdftools::pdf_ocr_text("table.pdf")
 
-img
-
-#image_read("table.png") %>% image_ocr() %>% read_tsv()
-
-temp.data <- img %>%
-  image_ocr()
-
-temp.data %>%
-  stri_split(fixed = "\n") %>%
-  purrr::map(~ stringi::stri_split(str = ., fixed = "‘")) %>%
-  .[[1]] %>%
-  purrr::map_df(~ tibble::tibble(name = .[1], 
-                                 weight = .[2], 
-                                 crossload_Kn = .[3],
-                                 result_Kn = .[4], 
-                                 pa = .[5])) %>%
-  dplyr::glimpse()
+cat(x)
+x
+read_tsv(x)
+# # preprocessing
+# img <- image_read("table.png") %>% 
+#   image_crop(geometry_area(y = 35))  %>% 
+#   image_transparent("white", fuzz=50) %>% 
+#   image_background("white") %>%
+#   image_negate() %>%
+#   image_morphology(method = "Thinning", kernel = "Rectangle:20x1+0+0^<") %>%
+#   image_negate()
+# 
+# img
+# 
+# #image_read("table.png") %>% image_ocr() %>% read_tsv()
+# 
+# temp.data <- img %>%
+#   image_ocr()
+# 
+# temp.data %>%
+#   stri_split(fixed = "\n") %>%
+#   purrr::map(~ stringi::stri_split(str = ., fixed = "‘")) %>%
+#   .[[1]] %>%
+#   purrr::map_df(~ tibble::tibble(name = .[1], 
+#                                  weight = .[2], 
+#                                  crossload_Kn = .[3],
+#                                  result_Kn = .[4], 
+#                                  pa = .[5])) %>%
+#   dplyr::glimpse()
