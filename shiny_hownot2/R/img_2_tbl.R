e75671d80036261736146bc2587f350c4825b77c
Tim Bender <bendertj@gmail.com>
2023-10-16 18:54:02 -0400

    Add files via upload

--- shiny_hownot2/R/img_2_tbl.R
+++ shiny_hownot2/R/img_2_tbl.R
@@ -22,72 +22,75 @@ wd <- list(img    = "C:/Users/bende/Documents/R/play/shiny_misc/shiny_hownot2/da
            output = "C:/Users/bende/Documents/R/play/shiny_misc/shiny_hownot2/output", 
            shiny  = NA)
 
-
-# get image----
-setwd(wd$img)
-
-df.text.fixes <- data.frame(from1 = c("CAMP Nano 22", "\\|"), 
-                            to1   = c("CAMP Nano22", "I"))
-colnames.out <- c("name", "weight_g", "crossload_mbs_Kn", 
-              "result_Kn", "pulled_against")
-skip_n <- 3
-img.title <- "cross_loading_carabiners"
-img.url   <- "https://static.wixstatic.com/media/c990e4_1f596631067f4cf38c9e79a6a72ba0de~mv2.jpg"
-
-# # download file
-# download.file(img.url, destfile = paste0(img.title,".jpg"))    # for archive
-# file.copy(from = paste0(img.title,".jpg"), to   = "table.jpg", 
-#           overwrite = ) # for processing
-
-file.copy("cross_loading_carabiners.jpg", "table.jpg", overwrite = T)
-
-# read image
-img <- image_read("table.jpg") %>% 
-  image_ocr() #%>%
-  # image_crop(geometry_area(y = 35))  %>%
-  # image_transparent("white", fuzz=50) %>%
-  # image_background("white") %>%
-  # image_negate() %>%
-  # image_morphology(method = "Thinning", kernel = "Rectangle:20x1+0+0^<") %>%
-  # image_negate()
-
-# preprocessing
-
-for(i in 1:nrow(df.text.fixes)){
-  img <- gsub(pattern = df.text.fixes$from1[i], 
-       replacement = df.text.fixes$to1[i], 
-       x = img)
+# Funs----
+pd <- function(img.filename, 
+               find.replace = data.frame(from = c(NA), to = c(NA)), 
+               colnames.out = NA, 
+               skip_n = 0
+){
+  require(readr)
+  require(dplyr)
+  file.copy(img.filename, "table.jpg", overwrite = T)
+  # read image
+  img <- image_read("table.jpg") %>% 
+    image_ocr() 
+  # preprocessing
+  for(i in 1:nrow(find.replace)){
+    img <- gsub(pattern = find.replace$from[i], 
+                replacement = find.replace$to[i], 
+                x = img)
+  }
+  data1 <- img %>%
+    read_lines() %>%
+    .[!. %in% c("")]
+  # processing
+  data.out <- gsub(pattern = "(?<=\\d) | (?=\\d)", 
+                   replacement = "_", 
+                   x = data1, perl = T) %>%
+    #gsub(pattern = " \\(.*\\)$", "", .) %>%
+    strsplit(., "_") %>%
+    lapply(., paste0, collapse = ",") %>%
+    unlist() %>%
+    .[(1:length(.)) > skip_n] %>%
+    paste0(., collapse = "\n") %>%
+    read_csv(., col_names = F)
+  # name columns
+  colnames(data.out) <- colnames.out
+  data.out$filename <- img.filename
+  # return
+  return(data.out)
 }
 
+# get image----
 
-data1 <- img %>%
-  read_lines() %>%
-  .[!. %in% c("")]
-
-
+# process images
+setwd(wd$img)
 
 
-data.out <- gsub(pattern = "(?<=\\d) | (?=\\d)", 
-     replacement = "_", 
-     x = data1, perl = T) %>%
-  gsub(pattern = " \\(.*\\)$", "", .) %>%
-  strsplit(., "_") %>%
-  lapply(., paste0, collapse = ",") %>%
-  unlist() %>%
-  .[(1:length(.)) > skip_n] %>%
-  paste0(., collapse = "\n") %>%
-  read_csv(., col_names = F)
+# cross loading carabiners----
+df_fr <- data.frame(from = c("CAMP Nano 22", "\\|", " \\(3.+tries)"), 
+                    to   = c("CAMP Nano22", "I", " "))
+co_v <- c("name", "weight_g", "crossload_mbs_Kn", 
+          "result_Kn", "pulled_against")
+skip_rows <- 3
+img.title <- "cross_loading_carabiners.jpg"
 
-colnames(data.out) <- colnames.out
 
-data.out
 
-ggplot(data = data.out, 
-       aes(x = crossload_mbs_Kn, y = result_Kn))+
-  geom_point()+
-  geom_function(fun = function(x) x , 
-                linetype = 2, 
-                aes(color = ("safe line")))+
-  xlim(0,NA)+
-  ylim(0,NA)
+dfout_crossloadcarabiners <- pd(img.filename = img.title, 
+                                find.replace = df_fr, 
+                                colnames.out = co_v, 
+                                skip_n = skip_rows)
+
+
+# save as RData----
+
+setwd(wd$output)
+save(list = ls(pattern = "^dfout_"), 
+     file = "hownot2.RData")
+setwd(wd$img)
+
+# remove files
+
+
 
