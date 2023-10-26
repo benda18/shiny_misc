617ebd54b31cc3d1ebc5aa223e8a317d507c4ef0
Tim Bender <bendertj@gmail.com>
2023-10-26 17:20:48 -0400

    Add files via upload

--- shiny_flightsim_journey/R/calc_distances.R
+++ shiny_flightsim_journey/R/calc_distances.R
@@ -22,12 +22,18 @@ setwd("~/R/play/shiny_misc/shiny_flightsim_journey")
 wd <- list(data =   "~/R/play/shiny_misc/shiny_flightsim_journey/data", 
            output = "~/R/play/shiny_misc/shiny_flightsim_journey/output", 
            R =      "~/R/play/shiny_misc/shiny_flightsim_journey/R", 
-           shiny =  "~/R/play/shiny_misc/shiny_flightsim_journey/shiny")
+           shiny =  "~/R/play/shiny_misc/shiny_flightsim_journey/shiny/flight_sim_cyoa")
 
 setwd(wd$output)
 
 # load data----
 load("airport_data.RData")
+
+# setwd(wd$shiny)
+# save(cw_cont.country, 
+#      data_airports, 
+#      data_runways, 
+#      file = "shinyData.RData")
 
 # Define the function
 calculate_distance <- function(lon1, lat1, lon2, lat2) {
@@ -35,25 +41,24 @@ calculate_distance <- function(lon1, lat1, lon2, lat2) {
   distHaversine(c(lon1, lat1), c(lon2, lat2))
 }
 
-# new crosswalks----
-cw_cont.country <- data_countries %>%
-  group_by(continent, 
-           iso_country, 
-           country_name) %>%
-  summarise()
-cw_cont.country$continent <- factor(cw_cont.country$continent, 
-                                    levels = c("NA", "SA", "EU", "AF", "AS", "OC", "AN"))
-cw_cont.country$iso_country <- factor(cw_cont.country$iso_country, 
-                                      levels = unique(cw_cont.country$iso_country[order(cw_cont.country$country_name)]))
-cw_cont.country$country_name <- factor(cw_cont.country$country_name)
+# # new crosswalks----
+# cw_cont.country <- data_countries %>%
+#   group_by(continent, 
+#            iso_country, 
+#            country_name) %>%
+#   summarise()
+# cw_cont.country$continent <- factor(cw_cont.country$continent, 
+#                                     levels = c("NA", "SA", "EU", "AF", "AS", "OC", "AN"))
+# cw_cont.country$iso_country <- factor(cw_cont.country$iso_country, 
+#                                       levels = unique(cw_cont.country$iso_country[order(cw_cont.country$country_name)]))
+# cw_cont.country$country_name <- factor(cw_cont.country$country_name)
 
 # filters----
 
 # pick_continent
 
 # pick_country
-sort(unique(data_airports$iso_country))
-data_countries
+
 # pick_region
 # pick distance
 
@@ -112,7 +117,7 @@ valid.apts <- data_airports[data_airports$ident %in% valid.apts &
                 !is.na(data_airports$iata_code),]$ident
 
 some.apts <- sample(valid.apts,size=2,replace=F)
-
+#some.apts <- c("EGAC", "EIKY")
 
 
 rw.metadata <- data_runways[data_runways$airport_ident %in% some.apts,] %>%
@@ -124,19 +129,24 @@ rw.metadata <- data_runways[data_runways$airport_ident %in% some.apts,] %>%
            elevation = le_elevation_ft) %>%
   summarise()
 
-paste(scales::comma(rw.metadata$elevation), sep = ", ", collapse = ", ")
+
 
 ggplot(data = data_airports[data_airports$ident %in% some.apts,]) + 
   geom_sf(data = sf::st_as_sf(rnaturalearthdata::countries110), 
-          fill = "white", color = "white")+
-  geom_sf(data = sf::st_as_sf(rnaturalearthdata::coastline110), 
-          color = "grey")+
+          fill = "white", color = "grey")+
+  # geom_sf(data = sf::st_as_sf(rnaturalearthdata::coastline110), 
+  #         color = "grey")+
   geom_path(aes(x = longitude_deg, y = latitude_deg)) +
   geom_label_repel(min.segment.length = 0,
                    aes(x = longitude_deg, y = latitude_deg, 
                  label = ident))+
   geom_point(aes(x = longitude_deg, y = latitude_deg))+
-  coord_sf() +
+  #coord_sf() +
+  theme(panel.background = element_rect(fill = "powderblue"), 
+        plot.background = element_rect(fill = "light grey"), 
+        strip.background =  element_rect("red"), 
+        axis.line = element_blank(), 
+        panel.grid = element_line(color = "skyblue", linetype = 23))+
   labs(title = paste(data_airports[data_airports$ident %in% some.apts,]$name, 
                       collapse = " >>> "), 
   subtitle = glue("Elevation: {paste(scales::comma(rw.metadata$elevation), \"ft\",  collapse = \", \")}
--- shiny_flightsim_journey/shiny/flight_sim_cyoa/app.R
+++ shiny_flightsim_journey/shiny/flight_sim_cyoa/app.R
@@ -1,16 +1,53 @@
-
-
 library(shiny)
+library(dplyr)
+library(rnaturalearthdata)
+library(ggplot2)
+library(ggrepel)
 
+# load data----
+load("shinyData.RData")
+
+calculate_distance <- function(lon1, lat1, lon2, lat2) {
+  require(geosphere)
+  distHaversine(c(lon1, lat1), c(lon2, lat2))
+}
+
+# pre-filter----
+#data_airports <- 
+data_airports <- data_airports %>%
+  .[!.$type %in% c("closed", "balloonport", "heliport", "seaplane_base"),] %>%
+  .[nchar(.$ident) == 4,] %>%
+  .[!is.na(.$elevation_ft),]
+
+data_runways <- data_runways %>%
+  .[.$airport_ident %in% data_airports$ident,] %>%
+  .[.$closed == 0,] %>%
+  .[grepl("^asp|^con", .$surface, ignore.case = T),] %>%
+  .[!grepl("-", .$surface, ignore.case = T),] %>%
+  .[.$lighted == 1,] %>%
+  .[!is.na(.$le_longitude_deg) & 
+     !is.na(.$le_latitude_deg) & 
+     !is.na(.$he_longitude_deg) & 
+     !is.na(.$he_latitude_deg),] %>%
+  group_by(airport_ident) %>%
+  slice_max(., order_by = length_ft, n = 1)
+
+  
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Title"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
+          # Maximum Plane Range
+          sliderInput(inputId = "planeRange", 
+                      "Max Plane Range (mi)", 
+                      min = 0, 
+                      max = 10000, 
+                      value = 100),
           # elevation filter
             sliderInput(inputId = "runwayElev",
                         "Runway Elevation (ft)",
@@ -23,29 +60,19 @@ ui <- fluidPage(
                         min = 0, 
                         max = 30000, 
                         value = c(100,4000)),
-            # surface type filter
-            checkboxGroupInput(inputId = "surfaceType", 
-                               label = "Runway Surface Type(s)", 
-                               choices = NULL, 
-                               selected = NULL, 
-                               inline = F, 
-                               width = NULL, 
-                               choiceNames = NULL, 
-                               choiceValues = NULL), 
             # continent
-            checkboxGroupInput(inputId = "continentCB", 
-                               label = "Runway Surface Type(s)", 
+            checkboxGroupInput(inputId = "sel_continents", 
+                               label = "Continent", 
                                choices = NULL, 
-                               selected = NULL, 
+                               selected = c("NA"), 
                                inline = F, 
                                width = NULL, 
-                               choiceNames = NULL, 
-                               choiceValues = NULL),
-            # country
-            # interactive updateable checkbox group----
-            
+                               choiceNames = c("North America", "South America", "Europe", 
+                                               "Africa", "Asia", "Oceania", "Antarctica"), 
+                               choiceValues = c("NA", "SA", "EU", "AF", "AS", "OC", "AN"))
             # start airport
             # end airport
+            # click button to choose
         ),
 
         # Show a plot of the generated distribution
@@ -59,14 +86,44 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+      
+      #some.apts <- c("KCVG", "KLAX")
+      some.apts <- sample(data_airports$ident,2,replace=F)
+      
+      rw.metadata <- data_runways[data_runways$airport_ident %in% some.apts,] %>%
+        group_by(airport_ident) %>%
+        slice_max(., order_by = length_ft, n = 1) %>%
+        group_by(ident = airport_ident, 
+                 surface, 
+                 length_ft, 
+                 elevation = le_elevation_ft) %>%
+        summarise()
+      
+      out.plot <- ggplot(data = data_airports[data_airports$ident %in% some.apts,]) + 
+        geom_sf(data = sf::st_as_sf(rnaturalearthdata::countries110), 
+                fill = "white", color = "grey")+
+        geom_path(aes(x = longitude_deg, y = latitude_deg)) +
+        geom_label_repel(min.segment.length = 0,
+                         aes(x = longitude_deg, y = latitude_deg, 
+                             label = ident))+
+        geom_point(aes(x = longitude_deg, y = latitude_deg))+
+        #coord_sf() +
+        theme(panel.background = element_rect(fill = "powderblue"), 
+              plot.background = element_rect(fill = "light grey"), 
+              strip.background =  element_rect("red"), 
+              axis.line = element_blank(), 
+              panel.grid = element_line(color = "skyblue", linetype = 23))+
+        labs(title = paste(data_airports[data_airports$ident %in% some.apts,]$name,
+                           collapse = " >>> "),
+             subtitle = glue("Elevation: {paste(scales::comma(rw.metadata$elevation), \"ft\",  collapse = \", \")}
+  Runway Length: {paste(scales::comma(rw.metadata$length_ft),  \"ft\", collapse = \", \")}
+  Runway Surface: {paste(rw.metadata$surface,collapse = \", \")}
+  Flight Distance: {scales::comma(calculate_distance(lon1 = data_airports$longitude_deg[data_airports$ident == some.apts[1]],
+                   lat1 = data_airports$latitude_deg[data_airports$ident == some.apts[1]],
+                   lon2 = data_airports$longitude_deg[data_airports$ident == some.apts[2]],
+                   lat2 = data_airports$latitude_deg[data_airports$ident == some.apts[2]])/1609.34)} miles\n"))
+      
+      print(out.plot)
     })
 }
 
