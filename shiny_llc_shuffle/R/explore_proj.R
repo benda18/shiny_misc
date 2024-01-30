029c1c8d5448468ce8896beb82e4434a090eae1f
Tim Bender <bendertj@gmail.com>
2024-01-30 09:49:17 -0500

    Add files via upload

--- shiny_llc_shuffle/R/explore_proj.R
+++ shiny_llc_shuffle/R/explore_proj.R
@@ -1,34 +1,22 @@
 library(dplyr)
 library(readr)
 library(data.table)
+library(lubridate)
 
 
 rm(list=ls());cat('\f')
 
 # Funs----
-fun_dl.re.sales <- function(sale.yr){
-  require(data.table)
-  require(glue)
-  # data source for ref: 
-  # https://go.mcohio.org/applications/treasurer/search/filedownloads.cfm
-  cat("SOURCE: https://go.mcohio.org/applications/treasurer/search/filedownloads.cfm\n\n")
-  # error handling----
-  sale.yr <- as.character(sale.yr)
-  # must be 4 dig &  must be between 2023 and 2011
-  if(nchar(sale.yr) != 4 |  
-     !data.table::between(as.numeric(sale.yr), 2011, 2023)){
-    stop("ERROR: [sale.yr] must be between 2011 and 2023")
-  }
-  # generate url----
-  return(glue("https://go.mcohio.org/applications/treasurer/search/data/Yearly/SALES_{sale.yr}.zip"))
-}
 
 
 
 
 # working directory----
+
+
 long.wd <- "C:\\Users\\bende\\Documents\\R\\play\\shiny_misc\\shiny_llc_shuffle"
 short.wd <- "~/R/play/shiny_misc/shiny_llc_shuffle"
+setwd(short.wd)
 
 # ?path.expand()
 # ?file.path()
@@ -43,11 +31,52 @@ mc.delinquient.tax <- "https://go.mcohio.org/applications/treasurer/search/fdpop
 mc.sales.annual    <- "https://go.mcohio.org/applications/treasurer/search/fdpopup.cfm?dtype=YS"
 
 
-# DOWNLOAD DATA----
-# dl real estate sales
+# download data---
+# done manually
+
+
+# Sales Data Tidy----
+setwd("data")
+files.res.sales <- list.files(pattern = "^SALES_\\d{4,4}_RES\\.csv$")
+
+sales.res.2023 <- read_csv(file = grep(pattern = "2023", x = files.res.sales, value = T), 
+                           #col_types = "c",
+                           guess_max = min(c(10000, Inf)))
+
+sales.res.2023$SALEDTE <- dmy(sales.res.2023$SALEDTE)
+#sales.res.2023$CONVNUM <- as.numeric(sales.res.2023$CONVNUM)  #no NAs result but not ideal
+sales.res.2023$PRICE   <- as.numeric(sales.res.2023$PRICE)
+sales.res.2023$ACRES   <- as.numeric(sales.res.2023$ACRES)
+
+sales.res.2023$SALETYPE %>% unique()
+sales.res.2023$SALEVALIDITY %>% unique()
+
+sales.res.2023$sale_valid <- grepl("^DESIGNATED|^VALID |^LAND CONTRACT|^LIQUIDATION|^PARTIAL|^RELATED|^Mobile |^SALE INVOLVING", 
+                                   x = sales.res.2023$SALEVALIDITY)
+
+sales.res.2023 %>%
+  group_by(sale_valid,SALEVALIDITY ) %>%
+  summarise()
 
 
 
 
 
+grep(pattern = "OWN|NAME", 
+     x = colnames(sales.res.2023), 
+     value = T, 
+     ignore.case = T)
+
+grep(pattern = "deed|conv", 
+     x = colnames(sales.res.2023), 
+     value = T, 
+     ignore.case = T)
+
+grep(pattern = "sale", 
+     x = colnames(sales.res.2023), 
+     value = T, 
+     ignore.case = T)
+
+# Delinquent Tax Data Tidy----
+files.deltax.all <- list.files(pattern = "^Delq_\\d{8,8}\\.csv$")
 
