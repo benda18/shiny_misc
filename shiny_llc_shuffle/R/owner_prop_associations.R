6c283be236934e8eeae7b6ed0879cc5d71167a4d
Tim Bender <bendertj@gmail.com>
2024-02-01 14:44:54 -0500

    Add files via upload

--- shiny_llc_shuffle/R/owner_prop_associations.R
+++ shiny_llc_shuffle/R/owner_prop_associations.R
@@ -96,57 +96,51 @@ M_own2parid <- rbind(summarise(group_by(master_sales.23, owner = own1, PARID)),
   group_by_all() %>%
   summarise()
 
+M_own2pardate_id <- rbind(summarise(group_by(master_sales.23, owner = own1, pardate_id)),
+                          summarise(group_by(master_sales.23, owner = own2, pardate_id))) %>%
+  group_by_all() %>%
+  summarise()
+
 # build graph
 GR_own2own       <- graph_from_data_frame(d = M_own2own, directed = F)
+GR_own2parid     <- graph_from_data_frame(d = M_own2parid, directed = F)
+GR_own2pardate_id <- graph_from_data_frame(d = M_own2pardate_id, directed = F)
 
 igraph::gsize(GR_own2own)  # number of edges of graph
+igraph::gsize(GR_own2parid)
+igraph::gsize(GR_own2pardate_id)
 
 clusters_own2own <- clusters(GR_own2own)
-groups_own2own   <- groups(clusters_own2own)
+#groups_own2own   <- groups(clusters_own2own)
 
 clusters_own2own$membership # name = owner, value = cluster_id
 clusters_own2own$csize      # cluster_id number of members
 clusters_own2own$no         # total number of clusters
 
 CW_owner.clusterid <- data.frame(owner      = names(clusters_own2own$membership), 
-                                 cluster_id = unname(clusters_own2own$membership)) %>% as_tibble()
+                                 cluster_id = as.integer(unname(clusters_own2own$membership))) %>% 
+  as_tibble()
 
-M_clusterid_size  <- data.frame(cluster_id = 1:length(clusters_own2own$csize), 
-                                n_owners   = clusters_own2own$csize) %>% as_tibble()
+M_clusterid_size  <- data.frame(cluster_id = as.integer(1:length(clusters_own2own$csize)), 
+                                n_owners   = clusters_own2own$csize) %>% 
+  as_tibble()
+
+CW_parid.clusterid       <- clusters
+CW_pardate_id.clusterid
 
 
-# temp1 <- igraph::clusters(GR_own2own) %>% 
-#   groups() %>% 
-#   #length() # tells you how many cluster groups
-#   summary() %>% 
-#   as.data.table() %>% as.data.frame() %>% as_tibble()
-# 
-# temp1$V2 %>% table
-# temp1$V2 %>% unique
-# temp1$V1 %>% table %>% unname %>% table
-# temp1$V1 %>% as.numeric() %>% is.na() %>% table()
-# temp1$V1 %>% summary
-# temp1$V1 %>% as.numeric %>% summary
-# temp1$V1 %>% as.numeric %>% fivenum
-# temp1$V1 %>% length
-# 
-# temp1$N %>% as.numeric() %>% is.na() %>% table(., useNA = "always")
-# temp1$N %>% as.numeric() %>% is.na() %>% as.numeric() %>% plot(type = "l")
-# 
-# temp1$N %>% unique
-# 
-# temp1 <- temp1 %>%
-#   as.data.table(.) %>%
-#   dcast(.,V1 ~ V2) %>%
-#   as.data.frame() %>%
-#   as_tibble() %>%
-#   mutate(., 
-#          cluster_id = as.numeric(V1), 
-#          n_owners   = as.numeric(Length)) %>%
-#   .[order(.$cluster_id),
-#     c("cluster")]
-# 
-# temp1
+rm(clusters_own2own)
+
+
+# review
+CW_owner.clusterid
+M_clusterid_size
+
+M_own2own
+M_own2parid
+
+
+
 
 
 # ASSOC:  OWNER <--> PROPERTY ----
