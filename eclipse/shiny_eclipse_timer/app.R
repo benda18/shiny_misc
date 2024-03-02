76b2b58306d17815e4d17e9b360c4596eca9a40d
Tim Bender <bendertj@gmail.com>
2024-03-02 16:15:12 -0500

    Update app.R

--- eclipse/shiny_eclipse_timer/app.R
+++ eclipse/shiny_eclipse_timer/app.R
@@ -36,20 +36,28 @@ ui <- fluidPage(
                      label   = "SEARCH")
       ),
       wellPanel(
-        fluidRow(
-          column(5,"longitude:"),
-          column(5,"latitude:")), 
-        fluidRow(
-          column(5, shiny::textOutput("coord_lon")), 
-          column(5, shiny::textOutput("search_lonlat"))),
+        fluidRow("Lon/Lat:"
+                 # column(5,"longitude:"),
+                 # column(5,"latitude:")
+        ), 
+        fluidRow(shiny::textOutput("coord_lon")
+                 # column(5, shiny::textOutput("coord_lon")), 
+                 # column(5, shiny::textOutput("coord_lat"))
+        ),
         
         # shiny::textInput(inputId = "lon_in",
         #                  label = "Longitude",
         #                  value = cordx),
       ),
-      wellPanel(fluidRow("External Web Resources"),
-                fluidRow("https://science.nasa.gov/eclipses/future-eclipses/eclipse-2024/where-when/"))
-     
+      wellPanel(
+        fluidRow("Eclipse Info"), 
+        fluidRow(
+          shiny::textOutput(outputId = "eclipse_info")
+        )
+      )
+      # wellPanel(fluidRow("External Web Resources"),
+      #           fluidRow("https://science.nasa.gov/eclipses/future-eclipses/eclipse-2024/where-when/"))
+      
     ),
     
     
@@ -63,39 +71,27 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  search_lonlat <- eventReactive(input$cxy_go, {
-    renderText({
-      #censusxy::cxy_oneline(address = input$addr_in)[,c("coordinates.x")]
-      "foo"
-      
-    })
-  })
   
+  cen_lon <- eventReactive(eventExpr = input$cxy_go, {
+    paste(round(censusxy::cxy_oneline(address = input$addr_in)[,c("coordinates.x", "coordinates.y")], 
+                5), 
+          sep = ", ", collapse = ", ")
+  })
+  # cen_lat <- eventReactive(eventExpr = input$cxy_go, {
+  #   #censusxy::cxy_oneline(address = input$addr_in)[,c("coordinates.y")]
+  # })
   output$coord_lon <- renderText({
-    search_lonlat
+    cen_lon()
   })
-    
+  # output$coord_lat <- renderText({
+  #   cen_lat()
+  # })
   
-    # output$coord_lon <- renderText({
-    #   cordx <- search_lonlat
-    # })
-    # 
-    # output$coord_lat <- renderText({
-    #   cordy <- search_lonlat
-    # })
-     
-    # output$coord_lon <- renderText({
-    #     cordx <- censusxy::cxy_oneline(address = input$addr_in)[,c("coordinates.x")]
-    # })
-    # output$coord_lat <- renderText({
-    #   cordy <- censusxy::cxy_oneline(address = input$addr_in)[,c("coordinates.y")]
-    # })
-   
- 
+  output$eclipse_info <- renderText({
+    "Foo"
+  })
   
   
-   
-
 }
 
 # Run the application 
