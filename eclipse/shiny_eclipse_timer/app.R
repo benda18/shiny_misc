ef00c26ae593f64cede29594edf1fe82d2650474
Tim Bender <bendertj@gmail.com>
2024-03-04 13:25:03 -0500

    Update app.R

--- eclipse/shiny_eclipse_timer/app.R
+++ eclipse/shiny_eclipse_timer/app.R
@@ -40,17 +40,19 @@ ui <- fluidPage(
                  shiny::numericInput(inputId = "lat_in", 
                                      label = "Enter Latitude (y)", 
                                      value = 39.92953)),
-        # fluidRow(actionButton(inputId = "eclipse_go", 
-        #                        label   = "SUBMIT"), ),
+        fluidRow(actionButton(inputId = "eclipse_go",
+                              label   = "SUBMIT"), ),
         fluidRow(shiny::textOutput("total_TF"))
       ),
       wellPanel(
         fluidRow("Eclipse Info"),
-        fluidRow(shiny::tableOutput(outputId = "eclipse_info"))),
-      wellPanel(
-        fluidRow("Maximum Sun Blocked (%):"),
-        fluidRow(shiny::textOutput(outputId = "totality_dur"))
-      )
+        #fluidRow(shiny::tableOutput(outputId = "eclipse_info"))
+        fluidRow(shiny::tableOutput(outputId = "return_eclipsego"))
+      ), 
+      # wellPanel(
+      #   fluidRow("Maximum Sun Blocked (%):"),
+      #   fluidRow(shiny::textOutput(outputId = "totality_dur"))
+      # )
     ),
     
     # Map----
@@ -95,36 +97,56 @@ server <- function(input, output) {
   #   }
   # })
   
+  # NEW MAP STUFF----
+  try(gen_map <- eventReactive(eventExpr = input$eclipse_go, {
+    #try(
+    ggplot() + 
+      geom_sf(data = okistates)+
+      geom_point(aes(x = input$lon_in, 
+                     y = input$lat_in), 
+                 color = "red", shape = "x", size = 8, fill = "red")+
+      theme(plot.background = element_rect(fill = "cyan"), 
+            text = element_text(size = 18))+
+      scale_x_continuous(name = NULL)+
+      scale_y_continuous(name = NULL)
+    #)
+  }))
+  try(output$okiMap <- renderPlot(width = 800, 
+                                  height = "auto", 
+                                  {
+                                    gen_map()
+                                  }))
+  # /NEW MAP STUFF----
   
   
-  output$okiMap <- renderPlot(width = 800, 
-                              height = "auto", 
-                              {
-                                try(
-                                  #print(
-                                    ggplot() + 
-                                      geom_sf(data = okistates)+
-                                      geom_point(aes(x = input$lon_in, 
-                                                     y = input$lat_in), 
-                                                 color = "red", shape = "x", size = 8, fill = "red")+
-                                      theme(plot.background = element_rect(fill = "cyan"), 
-                                            text = element_text(size = 18))+
-                                      scale_x_continuous(name = NULL)+
-                                      scale_y_continuous(name = NULL)
-                                  #)
-                                )
-                              })
+  # # OLD MAP STUFF----
+  # output$okiMap <- renderPlot(width = 800, 
+  #                             height = "auto", 
+  #                             {
+  #                               #try(
+  #                               ggplot() + 
+  #                                 geom_sf(data = okistates)+
+  #                                 geom_point(aes(x = input$lon_in, 
+  #                                                y = input$lat_in), 
+  #                                            color = "red", shape = "x", size = 8, fill = "red")+
+  #                                 theme(plot.background = element_rect(fill = "cyan"), 
+  #                                       text = element_text(size = 18))+
+  #                                 scale_x_continuous(name = NULL)+
+  #                                 scale_y_continuous(name = NULL)
+  #                               #)
+  #                             })
+  # # /OLD MAP STUFF----
+  
   
   # cen_lon button----
+  ## SHOW WHAT HAPPENS WHEN YOU add another thing reacting to the same button
   # cen_rando <- eventReactive(eventExpr = input$cxy_go, {
   #   sample(letters, 1)
   # })
   # output$rando_letter <- renderText({
   #   cen_rando()
   # })
-  
-  
-  
+  ## /
   cen_lon <- eventReactive(eventExpr = input$cxy_go, {
     paste(
       round(
@@ -136,31 +158,21 @@ server <- function(input, output) {
     cen_lon()
   })
   # /cen_lon button----
- 
+  
   
   # get eclipse times----
   
-  # submit_coords <- eventReactive(eventExpr = input$eclipse_go, {
-  #   #"do the stuff here"
-  # })
-  # 
-  # output$return_eclipsego <- renderTable({
-  #   submit_coords()
-  # })
-  
-  output$eclipse_info <- renderTable({
-    # ECLIPSE MATH----
-    Sys.sleep(2)
-    
+  # ADDED TO REPLACE BELOW----
+  submit_coords <- eventReactive(eventExpr = input$eclipse_go, {
     lon_in <- input$lon_in #-81.44067000
     lat_in <- input$lat_in # 41.24006000
     
-    # set date_time of eclipse (ideally before solar eclipse begins)----
+    # set date_time of eclipse (ideally before solar eclipse begins)
     
     greg_dt.local <- ymd_hm("2024-04-07 08:30AM", tz = "America/New_York")
     tz.local      <- tz(greg_dt.local)
     
-    # do the time conversions----
+    # do the time conversions
     # convert to utc
     greg_dt.utc <- with_tz(greg_dt.local, tz = "UTC")
     jul_dt.utc  <- swephR::swe_julday(year  = year(greg_dt.utc), 
@@ -174,18 +186,18 @@ server <- function(input, output) {
     ewl_out     <- swephR::swe_sol_eclipse_when_loc(jd_start  = jul_dt.utc, 
                                                     ephe_flag = 4, 
                                                     geopos    = c(#x = lon_in, 
-                                                                  x = ifelse(is.na(lon_in) | is.null(lon_in), 
-                                                                             -81.44067000, lon_in),
-                                                                  #y = lat_in, 
-                                                                  y = ifelse(is.na(lat_in) | is.null(lat_in), 
-                                                                             41.24006000, lat_in),
-                                                                  z = 10), 
+                                                      x = ifelse(is.na(lon_in) | is.null(lon_in), 
+                                                                 -81.44067000, lon_in),
+                                                      #y = lat_in, 
+                                                      y = ifelse(is.na(lat_in) | is.null(lat_in), 
+                                                                 41.24006000, lat_in),
+                                                      z = 10), 
                                                     backward = F)
     
     ewl_out$tret <- ewl_out$tret[1:5]
     ewl_out$attr <- ewl_out$attr[1+c(2)]
     
-    # out_times----
+    # out_times
     out.times <- data.frame(time_val.jul     = ewl_out$tret, 
                             local_time       = NA)
     
@@ -200,10 +212,10 @@ server <- function(input, output) {
                  #format = "%I:%M:%S%p %Z",
                  tz = tz.local) %>%
         as.character() #%>%
-        #tolower() %>%
-        # gsub("^0", "", .) %>%
-        # gsub("AM ", "am ", .) %>%
-        # gsub("PM ", "pm ", .)
+      #tolower() %>%
+      # gsub("^0", "", .) %>%
+      # gsub("AM ", "am ", .) %>%
+      # gsub("PM ", "pm ", .)
     }
     
     out.times <- out.times[order(out.times$local_time),]
@@ -215,7 +227,7 @@ server <- function(input, output) {
     rownames(out.times) <- 1:nrow(out.times)
     out.times <- out.times[,c("eclipse_event", "local_time")]
     
-    # out_attributes----
+    # out_attributes
     out.attr <- data.frame(longitude = lon_in, 
                            latitude  = lat_in,
                            total_ecl_at_loc = ewl_out$attr > 1)
@@ -230,7 +242,7 @@ server <- function(input, output) {
       out.times$local_time <- "partial eclipse only"
     }else{
       out.times$duration_minutes <- scales::comma(c(0,
-                                   diff(ymd_hms(out.times$local_time))),0.1)
+                                                    diff(ymd_hms(out.times$local_time))),0.1)
       out.times$local_time <- out.times$local_time %>%
         gsub("^.*-\\d{2,2} ", "", .) %>%
         gsub("^0", "", .) %>%
@@ -241,37 +253,136 @@ server <- function(input, output) {
     out.times
   })
   
-  # get totality duration----
-  output$totality_dur <- renderText({
-    # ECLIPSE MATH----
-    
-    lon_in <- input$lon_in #-81.44067000
-    lat_in <- input$lat_in # 41.24006000
-    
-    # set date_time of eclipse (ideally before solar eclipse begins)----
-    greg_dt.local <- ymd_hm("2024-04-07 08:30AM", tz = "America/New_York")
-    tz.local      <- tz(greg_dt.local)
-    
-    # do the time conversions----
-    # convert to utc
-    greg_dt.utc <- with_tz(greg_dt.local, tz = "UTC")
-    jul_dt.utc  <- swephR::swe_julday(year  = year(greg_dt.utc), 
-                                      month = lubridate::month(greg_dt.utc, label = F), 
-                                      day   = mday(greg_dt.utc), 
-                                      hourd = hour(greg_dt.utc) + 
-                                        (minute(greg_dt.utc)/60) + 
-                                        (second(greg_dt.utc)/60/60), 
-                                      gregflag = 1)
-    
-    ewl_out     <- swephR::swe_sol_eclipse_when_loc(jd_start  = jul_dt.utc, 
-                                                    ephe_flag = 4, 
-                                                    geopos    = c(x = lon_in, 
-                                                                  y = lat_in, 
-                                                                  z = 10), 
-                                                    backward = F)$attr[3]
-    scales::percent(ewl_out,0.00000001)
-    
+  output$return_eclipsego <- renderTable({
+    submit_coords()
   })
+  # /ADDED TO REPLACE BELOW----
+  
+  # # TO COMMENT OUT ----
+  # output$eclipse_info <- renderTable({
+  #   # ECLIPSE MATH
+  #   lon_in <- input$lon_in #-81.44067000
+  #   lat_in <- input$lat_in # 41.24006000
+  #   
+  #   # set date_time of eclipse (ideally before solar eclipse begins)
+  #   
+  #   greg_dt.local <- ymd_hm("2024-04-07 08:30AM", tz = "America/New_York")
+  #   tz.local      <- tz(greg_dt.local)
+  #   
+  #   # do the time conversions
+  #   # convert to utc
+  #   greg_dt.utc <- with_tz(greg_dt.local, tz = "UTC")
+  #   jul_dt.utc  <- swephR::swe_julday(year  = year(greg_dt.utc), 
+  #                                     month = lubridate::month(greg_dt.utc, label = F), 
+  #                                     day   = mday(greg_dt.utc), 
+  #                                     hourd = hour(greg_dt.utc) + 
+  #                                       (minute(greg_dt.utc)/60) + 
+  #                                       (second(greg_dt.utc)/60/60), 
+  #                                     gregflag = 1)
+  #   
+  #   ewl_out     <- swephR::swe_sol_eclipse_when_loc(jd_start  = jul_dt.utc, 
+  #                                                   ephe_flag = 4, 
+  #                                                   geopos    = c(#x = lon_in, 
+  #                                                                 x = ifelse(is.na(lon_in) | is.null(lon_in), 
+  #                                                                            -81.44067000, lon_in),
+  #                                                                 #y = lat_in, 
+  #                                                                 y = ifelse(is.na(lat_in) | is.null(lat_in), 
+  #                                                                            41.24006000, lat_in),
+  #                                                                 z = 10), 
+  #                                                   backward = F)
+  #   
+  #   ewl_out$tret <- ewl_out$tret[1:5]
+  #   ewl_out$attr <- ewl_out$attr[1+c(2)]
+  #   
+  #   # out_times
+  #   out.times <- data.frame(time_val.jul     = ewl_out$tret, 
+  #                           local_time       = NA)
+  #   
+  #   for(i in 1:nrow(out.times)){
+  #     out.times$local_time[i] <- swephR::swe_jdet_to_utc(jd_et = ewl_out$tret[i], 
+  #                                                        gregflag = 1) %>%
+  #       paste(., sep = "-", collapse = "-") %>%
+  #       ymd_hms() %>%
+  #       with_tz(., tz.local) %>%
+  #       strftime(., 
+  #                format = "%m-%d-%y %I:%M:%S%p %Z", 
+  #                #format = "%I:%M:%S%p %Z",
+  #                tz = tz.local) %>%
+  #       as.character() #%>%
+  #       #tolower() %>%
+  #       # gsub("^0", "", .) %>%
+  #       # gsub("AM ", "am ", .) %>%
+  #       # gsub("PM ", "pm ", .)
+  #   }
+  #   
+  #   out.times <- out.times[order(out.times$local_time),]
+  #   out.times$eclipse_event <- c("begin_partial_eclipse", 
+  #                                "begin_total_eclipse", 
+  #                                "maximum_total_eclipse", 
+  #                                "end_total_eclipse", 
+  #                                "end_partial_eclipse")
+  #   rownames(out.times) <- 1:nrow(out.times)
+  #   out.times <- out.times[,c("eclipse_event", "local_time")]
+  #   
+  #   # out_attributes
+  #   out.attr <- data.frame(longitude = lon_in, 
+  #                          latitude  = lat_in,
+  #                          total_ecl_at_loc = ewl_out$attr > 1)
+  #   rownames(out.attr) <- 1:nrow(out.attr)
+  #   out.attr
+  #   # out.times$lon <- out.attr$longitude
+  #   # out.times$lat <- out.attr$latitude
+  #   # out.times$t_ecl <- out.attr$total_ecl_at_loc
+  #   
+  #   if(!out.attr$total_ecl_at_loc){
+  #     #out.times$local_time[2:4] <- NA
+  #     out.times$local_time <- "partial eclipse only"
+  #   }else{
+  #     out.times$duration_minutes <- scales::comma(c(0,
+  #                                  diff(ymd_hms(out.times$local_time))),0.1)
+  #     out.times$local_time <- out.times$local_time %>%
+  #       gsub("^.*-\\d{2,2} ", "", .) %>%
+  #       gsub("^0", "", .) %>%
+  #       gsub("AM ", "am ", .) %>%
+  #       gsub("PM ", "pm ", .)
+  #   }
+  #   
+  #   out.times
+  # })
+  ## /TO COMMENT OUT ----
+  
+  
+  # # get totality duration----
+  # output$totality_dur <- renderText({
+  #   # ECLIPSE MATH----
+  #   
+  #   lon_in <- input$lon_in #-81.44067000
+  #   lat_in <- input$lat_in # 41.24006000
+  #   
+  #   # set date_time of eclipse (ideally before solar eclipse begins)----
+  #   greg_dt.local <- ymd_hm("2024-04-07 08:30AM", tz = "America/New_York")
+  #   tz.local      <- tz(greg_dt.local)
+  #   
+  #   # do the time conversions----
+  #   # convert to utc
+  #   greg_dt.utc <- with_tz(greg_dt.local, tz = "UTC")
+  #   jul_dt.utc  <- swephR::swe_julday(year  = year(greg_dt.utc), 
+  #                                     month = lubridate::month(greg_dt.utc, label = F), 
+  #                                     day   = mday(greg_dt.utc), 
+  #                                     hourd = hour(greg_dt.utc) + 
+  #                                       (minute(greg_dt.utc)/60) + 
+  #                                       (second(greg_dt.utc)/60/60), 
+  #                                     gregflag = 1)
+  #   
+  #   ewl_out     <- swephR::swe_sol_eclipse_when_loc(jd_start  = jul_dt.utc, 
+  #                                                   ephe_flag = 4, 
+  #                                                   geopos    = c(x = lon_in, 
+  #                                                                 y = lat_in, 
+  #                                                                 z = 10), 
+  #                                                   backward = F)$attr[3]
+  #   scales::percent(ewl_out,0.00000001)
+  #   
+  # })
   
 }
 
